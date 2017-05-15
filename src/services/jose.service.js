27ed8ea1015f2cd5407929dd0d3f854b00bddf13
Billy He <qiushihe@me.com>
2017-05-15 00:24:39 -0700

    Implement jose service

---
+++ src/services/jose.service.js
@@ -0,0 +1,75 @@
+import Promise from "bluebird";
+import flow from "lodash/fp/flow";
+import isEmpty from "lodash/fp/isEmpty";
+import {JWK, JWS, util as joseUtil} from "node-jose";
+
+class JoseService {
+  constructor() {
+    this.keystore = JWK.createKeyStore();
+  }
+
+  verify(jws = {}) {
+    return Promise.resolve().then(() => {
+      const {
+        protected: protectedHeader,
+        payload,
+        signature
+      } = jws;
+
+      if (isEmpty(protectedHeader) || isEmpty(payload) || isEmpty(signature)) {
+        throw new Error("Invalid JWS");
+      }
+
+      const {
+        alg,
+        jwk,
+        kid,
+        nonce,
+        url
+      } = flow([
+        joseUtil.base64url.decode,
+        joseUtil.utf8.encode,
+        JSON.parse
+      ])(protectedHeader);
+
+      if (isEmpty(kid) && isEmpty(jwk)) {
+        throw new Error("Invalid header: Missing either kid or jwk");
+      }
+
+      if (!isEmpty(kid) && !isEmpty(jwk)) {
+        throw new Error("Invalid header: Can not have both kid and jwk");
+      }
+
+      if (isEmpty(alg) || isEmpty(nonce) || isEmpty(url)) {
+        throw new Error("Invalid header: Must have alg, nonce and url");
+      }
+
+      if (!isEmpty(kid)) {
+        const key = this.keystore.get(kid);
+        if (isEmpty(key)) {
+          throw new Error("Invalid header: Key not found for provided kid");
+        } else {
+          return key;
+        }
+      } else {
+        return this.keystore.add(jwk);
+      }
+    }).then(
+      (key) => JWS.createVerify(key).verify(jws)
+    ).then((result) => ({
+      ...result,
+      payload: JSON.parse(result.payload)
+    }));
+  }
+}
+
+let JoseServiceDefaultInstance = null;
+
+JoseService.GetDefaultInstance = (...args) => {
+  if (!JoseServiceDefaultInstance) {
+    JoseServiceDefaultInstance = new JoseService(...args);
+  }
+  return JoseServiceDefaultInstance;
+};
+
+export default JoseService;
---
+++ test/services/jose-service.spec.js
@@ -0,0 +1,158 @@
+import {expect} from "chai";
+import {JWK, JWS} from "node-jose";
+import get from "lodash/fp/get";
+import Promise from "bluebird";
+
+import JoseService from "services/jose.service";
+
+import {promiseIt} from "../test-helpers";
+
+const createSign = (fields, key, {hasJwk, hasKid}) => {
+  if (hasJwk && hasKid) {
+    return JWS.createSign(
+      {format: "flattened", fields: {...fields, jwk: key.toJSON()}},
+      {reference: "kid", key}
+    );
+  }
+
+  if (hasJwk && !hasKid) {
+    return JWS.createSign(
+      {format: "flattened", fields: {...fields, jwk: key.toJSON()}},
+      {reference: null, key}
+    );
+  }
+
+  if (!hasJwk && hasKid) {
+    return JWS.createSign(
+      {format: "flattened", fields: {...fields}},
+      {reference: "kid", key}
+    );
+  }
+
+  if (!hasJwk && !hasKid) {
+    return JWS.createSign(
+      {format: "flattened", fields: {...fields}},
+      {reference: null, key}
+    );
+  }
+};
+
+const sign = (header, payload, {hasJwk, hasKid}) => (key) => {
+  return createSign(header, key, {hasJwk, hasKid})
+    .update(JSON.stringify(payload))
+    .final();
+};
+
+describe("JoseService", () => {
+  let service;
+  let keystore;
+
+  beforeEach(() => {
+    service = new JoseService();
+    keystore = JWK.createKeyStore();
+  });
+
+  describe("#verify()", () => {
+    let header;
+    let payload;
+    let promisedKey;
+
+    beforeEach(() => {
+      header = {nonce: "lala", url: "http://lala.land/lala"};
+      payload = {lala: "LALA"};
+      promisedKey = keystore.generate("EC", "P-256");
+    });
+
+    promiseIt("should verify header and payload for new key", () => (
+      promisedKey
+        .then(sign(header, payload, {hasJwk: true, hasKid: false}))
+        .then((jws) => service.verify(jws))
+        .then(({payload: verifiedPayload, header: verifiedHeader}) => {
+          const {nonce: verifiedNonce, url: verifiedUrl} = verifiedHeader;
+          expect(verifiedNonce).to.equal(header.nonce);
+          expect(verifiedUrl).to.equal(header.url);
+          expect(verifiedPayload).to.deep.equal(payload);
+        })
+    ));
+
+    promiseIt("should verify header and payload for existing key", () => (
+      promisedKey
+        .then((key) => {
+          service.keystore.add(key);
+          return key;
+        })
+        .then(sign(header, payload, {hasJwk: false, hasKid: true}))
+        .then((jws) => service.verify(jws))
+        .then(({payload: verifiedPayload, header: verifiedHeader}) => {
+          const {nonce: verifiedNonce, url: verifiedUrl} = verifiedHeader;
+          expect(verifiedNonce).to.equal(header.nonce);
+          expect(verifiedUrl).to.equal(header.url);
+          expect(verifiedPayload).to.deep.equal(payload);
+        })
+    ));
+
+    promiseIt("should not accept header with invalid kid", () => (
+      promisedKey
+        .then(sign(header, payload, {hasJwk: false, hasKid: true}))
+        .then((jws) => service.verify(jws))
+        .then(() => {
+          const err = new Error("Invalid header accepted");
+          err._rethrow = true;
+          throw err;
+        })
+        .catch((err) => {
+          if (err._rethrow) {
+            throw err;
+          }
+        })
+    ));
+
+    promiseIt("should not accept header with both jwk and kid", () => (
+      promisedKey
+        .then(sign(header, payload, {hasJwk: true, hasKid: true}))
+        .then((jws) => service.verify(jws))
+        .then(() => {
+          const err = new Error("Invalid header accepted");
+          err._rethrow = true;
+          throw err;
+        })
+        .catch((err) => {
+          if (err._rethrow) {
+            throw err;
+          }
+        })
+    ));
+
+    promiseIt("should not accept header with neither jwk nor kid", () => (
+      promisedKey
+        .then(sign(header, payload, {hasJwk: false, hasKid: false}))
+        .then((jws) => service.verify(jws))
+        .then(() => {
+          const err = new Error("Invalid header accepted");
+          err._rethrow = true;
+          throw err;
+        })
+        .catch((err) => {
+          if (err._rethrow) {
+            throw err;
+          }
+        })
+    ));
+
+    promiseIt("should not accept header with missing fields", () => (
+      promisedKey
+        .then(sign({}, payload, {hasJwk: true, hasKid: false}))
+        .then((jws) => service.verify(jws))
+        .then(() => {
+          const err = new Error("Invalid header accepted");
+          err._rethrow = true;
+          throw err;
+        })
+        .catch((err) => {
+          if (err._rethrow) {
+            throw err;
+          }
+        })
+    ));
+  });
+});
