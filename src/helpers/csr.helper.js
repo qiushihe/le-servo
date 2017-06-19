3f29228fea3c2909f91b6d3d16e59295480dcfc3
Billy He <qiushihe@me.com>
2017-06-19 16:19:24 -0700

    Implement CSR helper and tests

---
+++ src/helpers/csr.helper.js
@@ -0,0 +1,49 @@
+import {pki as PKI} from "node-forge";
+import flow from "lodash/fp/flow";
+import filter from "lodash/fp/filter";
+import map from "lodash/fp/map";
+import get from "lodash/fp/get";
+import flatten from "lodash/fp/flatten";
+import uniq from "lodash/fp/uniq";
+import Promise from "bluebird";
+
+const stringReplace = (search, replacement) => (str) => str.replace(search, replacement);
+const stringToLowerCase = (str) => str.toLowerCase();
+const padMod4 = (pad) => (str) => (str.length % 4 !== 0 ? padMod4(pad)(`${str}=`) : str);
+
+export const parseCsr = (encodedCsr) => Promise.resolve().then(() => {
+  const pem = flow([
+    // Normalize encoded CSR string into base64 encoding
+    stringReplace(/-/g, "+"),
+    stringReplace(/_/g, "/"),
+    padMod4("="),
+    // CSR in ACME payload consists of only the base64 encoded string without the wrapping lines
+    // on either end.
+    (str) => (`-----BEGIN CERTIFICATE REQUEST-----\n${str}\n-----END CERTIFICATE REQUEST-----\n`)
+  ])(encodedCsr);
+
+  const {
+    subject: {attributes: subjectAttributes = []} = {},
+    attributes = []
+  } = PKI.certificationRequestFromPem(pem);
+
+  const commonNames = flow([
+    filter({name: "commonName"}),
+    map(flow([
+      get("value"),
+      stringToLowerCase
+    ]))
+  ])(subjectAttributes);
+
+  const extensionNames = flow([
+    filter({name: "extensionRequest"}),
+    map("extensions"),
+    flatten,
+    map("altNames"),
+    flatten,
+    filter({type: 2}),
+    map("value")
+  ])(attributes);
+
+  return uniq([...commonNames, ...extensionNames]);
+});
