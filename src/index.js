6e4a16de663b4b2f24b5b9bbec958d44f0333520
Billy He <qiushihe@me.com>
2017-07-22 15:02:14 -0700

    Update script with arguments

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import express from "express";
 
 import serverBuilder from "./server-builder";
 
-const port = 3000;
+const hostName = process.env.LE_SERVO_HOST_NAME || "localhost";
+const pathPrefix = process.env.LE_SERVO_PATH_PREFIX || "";
+const port = process.env.LE_SERVO_PORT || 3000;
+const nonceBufferSize = process.env.LE_SERVO_NONCE_BUFFER_SIZE || 32;
+
+const origin = `http://${hostName}:${port}${pathPrefix}`;
 
 const buildServer = serverBuilder({
-  origin: `http://localhost:${port}`,
-  nonceBufferSize: 32
+  origin,
+  nonceBufferSize
 });
 
-const server = buildServer(express());
-
-server.listen(port, () => {
-  console.log(`Server started on port ${port}`); // eslint-disable-line
+buildServer(express()).listen(port, () => {
+  console.log(`Server started on ${origin}`); // eslint-disable-line
 });
