5e1d82d9d6204028dc6f92f0364e23b290e3536a
Billy He <qiushihe@me.com>
2017-08-25 18:33:23 -0700

    Add missing rel="up" link to challenges related v1 proxy responses

--- src/v1/proxies/get-challenge-handler.proxy.js
+++ src/v1/proxies/get-challenge-handler.proxy.js
@@ -1,23 +1,32 @@
 import getChallenge from "src/handlers/challenge/get-challenge.handler";
 
 const proxiedGetChallengeHandler = (request) => {
-  const {challengeService} = request;
+  const {
+    directoryService,
+    challengeService
+  } = request;
 
   return getChallenge(request).then((respond) => {
     const {
       location,
+      links,
       body: {
         url,
         ...restResponseBody
       }
     } = respond;
 
+    const authorizationId = location.match(/\/authz\/([^\/]*)\/[^\/]*\/?$/)[1];
     const challengeId = location.match(/\/authz\/[^\/]*\/([^\/]*)\/?$/)[1];
 
     return challengeService.get(challengeId).then((challenge) => {
       return {
         ...respond,
         ...(challenge.processing ? {status: 202} : {}),
+        links: [
+          ...(links || []),
+          `${directoryService.getFullUrl(`/authz/${authorizationId}`)};rel="up"`
+        ],
         body: {
           ...restResponseBody,
           uri: url
