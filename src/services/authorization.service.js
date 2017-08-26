4d207350d4370c4f2915fe20ac8ed5d661e5be40
Billy He <qiushihe@me.com>
2017-08-25 18:32:18 -0700

    Create default http-01 challenge for new authorizations

--- src/services/authorization.service.js
+++ src/services/authorization.service.js
@@ -47,6 +47,11 @@ class AuthorizationService {
       return Promise.all([
         this.challengeService.create({
           authorizationId: authorization.id,
+          type: "http-01",
+          token: uuidV4().replace(/-/g, "")
+        }),
+        this.challengeService.create({
+          authorizationId: authorization.id,
           type: "tls-sni-01",
           token: uuidV4().replace(/-/g, "")
         })
